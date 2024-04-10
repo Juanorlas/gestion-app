4bef7714642ac5f3111b792cec5c27bbdcdb8165
juanorlas <juan7883@gmail.com>
2024-04-10 16:28:35 -0500

    Se crea listar usuarios

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import Login from "./components/layouts/auth/Login";
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from "./components/layouts/pages/Home";
+import ListarUsuarios from "./components/layouts/pages/usuarios/ListarUsuarios";
 let router = createBrowserRouter([
   {
     path: '/',
@@ -9,6 +10,10 @@ let router = createBrowserRouter([
   {
     path: '/home',
     element: <Home />
+  },
+  {
+    path: '/listar-usuarios',
+    element: <ListarUsuarios />
   }
 ])
 
---
+++ front/src/components/layouts/pages/usuarios/ListarUsuarios.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+let apiUsuarios = "http://localhost:5174/usuarios";
+
+const ListarUsuarios = () => {
+  const [usuarios, setUsuarios] = useState([]);
+
+  const getUsuarios = async () => {
+    let resultado = await axios.get(apiUsuarios);
+    setUsuarios(resultado.data);
+  };
+  console.log(usuarios);
+
+  return (
+    <div>
+      <button onClick={getUsuarios} type="button">
+        Listar Usuario
+      </button>
+      {usuarios.map((usuario) => {
+        <section>
+          <p>User: {usuario.usuario}</p>
+          <p>Contraseña: {usuario.contrasena}</p>
+          <p>Correo: {usuario.correo}</p>
+        </section>;
+      })}
+    </div>
+  );
+};
+
+export default ListarUsuarios;
