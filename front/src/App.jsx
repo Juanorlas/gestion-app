4ec5d015ed9a751c1c26c778bb4598370e926798
juanorlas <juan7883@gmail.com>
2024-04-10 15:19:45 -0500

    Se cre proyecto fs

---
+++ front/src/App.jsx
@@ -0,0 +1,24 @@
+import Login from "./components/layouts/auth/Login";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from "./components/layouts/pages/Home";
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+])
+
+
+function App() {
+  return (
+    <div className="login-container">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/database/dataBase.jsx
@@ -0,0 +1,12 @@
+export let usuarios = [
+  {
+    usuario: "juan",
+    contrasena: "1234",
+    correo: "juan7883@gmail.com",
+  },
+  {
+    usuario: "super-admin",
+    contrasena: "1234",
+    correo: "superadmin@gmail.com",
+  },
+];
